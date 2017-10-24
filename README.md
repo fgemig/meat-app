85b419582cc027accaec818b91332bf7f251451f
= <fabiogemignani@outlook.com>
2017-10-24 21:39:16 -0200

    primeiro commit :)

---
+++ README.md
@@ -0,0 +1,23 @@
+# meat-app - Curso de Angular 4 / Udemy
+
+### Instalando as Dependências
+
+`npm install`
+
+### Inicializando o Servidor
+
+`ng serve` ou `npm start`
+
+## 2. Iniciando o Backend
+
+### Instalando o json-server
+
+`npm install -g json-server`
+
+### Iniciando o serviço (raiz da aplicação)
+
+`json-server db.json`
+
+## Créditos
+
+Todas as imagens usadas na aplicação são pertencentes a freepik.com
---
+++ src/app/restaurant-detail/menu/menu.component.ts
@@ -0,0 +1,23 @@
+import { Observable } from 'rxjs/Observable';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { RestaurantService } from './../../restaurants/restaurant/restaurants.service';
+import { MenuItem } from './../menu-item/menu-item.model';
+
+@Component({
+  selector: 'mt-menu',
+  templateUrl: './menu.component.html'
+})
+export class MenuComponent implements OnInit {
+
+  menu: Observable<MenuItem>;
+
+  constructor(private restaurantService: RestaurantService,
+    private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.menu = this.restaurantService
+      .menuOfRestaurant(this.route.parent.snapshot.params['id']);
+  }
+
+}
